b132906107ff9228fdb4ac76746b527fecd8cc7f
Vlaho Srijense <vsrijense22@foi.hr>
2024-05-28 21:50:57 +0200

    Forma Narudzba Napravljena je forma za narudzbu

---
+++ Software/SCVZ Menza+/SCVZ Menza+/Narudzba.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SCVZ_Menza_
+{
+    public partial class Narudzba : Form
+    {
+        public Narudzba()
+        {
+            InitializeComponent();
+        }
+
+        private void brojNarudzbe_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Obrok_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Kolicina_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void statusNarudzbe_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void vrijemeNarudzbe_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gumbNaruci_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gumbOdustani_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
