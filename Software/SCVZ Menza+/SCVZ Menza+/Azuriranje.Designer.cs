21bceefd2f339a4535cbaba397f59e2c10ab2f1d
Vlaho Srijense <vsrijense22@foi.hr>
2024-05-28 21:59:16 +0200

    Forma Azuriranje Napravljena forma za azuriranje

---
+++ Software/SCVZ Menza+/SCVZ Menza+/Azuriranje.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace SCVZ_Menza_
+{
+    partial class Azuriranje
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.vrijemeNarudzbe = new System.Windows.Forms.TextBox();
+            this.statusNarudzbe = new System.Windows.Forms.TextBox();
+            this.Kolicina = new System.Windows.Forms.TextBox();
+            this.Obrok = new System.Windows.Forms.TextBox();
+            this.brojNarudžbe = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(432, 335);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 54);
+            this.button2.TabIndex = 35;
+            this.button2.Text = "Odustani";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(263, 334);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 56);
+            this.button1.TabIndex = 34;
+            this.button1.Text = "Ažuriraj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // vrijemeNarudzbe
+            // 
+            this.vrijemeNarudzbe.Location = new System.Drawing.Point(354, 244);
+            this.vrijemeNarudzbe.Name = "vrijemeNarudzbe";
+            this.vrijemeNarudzbe.Size = new System.Drawing.Size(173, 22);
+            this.vrijemeNarudzbe.TabIndex = 33;
+            this.vrijemeNarudzbe.TextChanged += new System.EventHandler(this.vrijemeNarudzbe_TextChanged);
+            // 
+            // statusNarudzbe
+            // 
+            this.statusNarudzbe.Location = new System.Drawing.Point(353, 197);
+            this.statusNarudzbe.Name = "statusNarudzbe";
+            this.statusNarudzbe.Size = new System.Drawing.Size(174, 22);
+            this.statusNarudzbe.TabIndex = 32;
+            this.statusNarudzbe.TextChanged += new System.EventHandler(this.statusNarudzbe_TextChanged);
+            // 
+            // Kolicina
+            // 
+            this.Kolicina.Location = new System.Drawing.Point(353, 151);
+            this.Kolicina.Name = "Kolicina";
+            this.Kolicina.Size = new System.Drawing.Size(174, 22);
+            this.Kolicina.TabIndex = 31;
+            this.Kolicina.TextChanged += new System.EventHandler(this.Kolicina_TextChanged);
+            // 
+            // Obrok
+            // 
+            this.Obrok.Location = new System.Drawing.Point(353, 105);
+            this.Obrok.Name = "Obrok";
+            this.Obrok.Size = new System.Drawing.Size(174, 22);
+            this.Obrok.TabIndex = 30;
+            this.Obrok.TextChanged += new System.EventHandler(this.Obrok_TextChanged);
+            // 
+            // brojNarudžbe
+            // 
+            this.brojNarudžbe.Location = new System.Drawing.Point(353, 60);
+            this.brojNarudžbe.Name = "brojNarudžbe";
+            this.brojNarudžbe.Size = new System.Drawing.Size(174, 22);
+            this.brojNarudžbe.TabIndex = 29;
+            this.brojNarudžbe.TextChanged += new System.EventHandler(this.brojNarudžbe_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(238, 247);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(115, 16);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Vrijeme narudžbe:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(244, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 16);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Status narudžbe:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(293, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 16);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Količina:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(303, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Obrok:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(257, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 16);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Broj narudžbe:";
+            // 
+            // Azuriranje
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.vrijemeNarudzbe);
+            this.Controls.Add(this.statusNarudzbe);
+            this.Controls.Add(this.Kolicina);
+            this.Controls.Add(this.Obrok);
+            this.Controls.Add(this.brojNarudžbe);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Azuriranje";
+            this.Text = "Azuriranje";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox vrijemeNarudzbe;
+        private System.Windows.Forms.TextBox statusNarudzbe;
+        private System.Windows.Forms.TextBox Kolicina;
+        private System.Windows.Forms.TextBox Obrok;
+        private System.Windows.Forms.TextBox brojNarudžbe;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
